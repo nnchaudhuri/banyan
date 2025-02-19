0e64fa639fa11f5f92a4cbae8411d3f5e8414c35
Nikhil Chaudhuri <nikhilnchaudhuri@gmail.com>
2025-02-19 15:40:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/banyan/",
+  base: '/banyan/',
   plugins: [tsconfigPaths(), react()]
 })
